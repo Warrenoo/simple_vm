2ca64688a9b733b1e354603c1f68389a0377a5ac
沈汪洋 <541991a@gmail.com>
2016-01-23 17:28:09 +0800

    fix

--- socket/client.c
+++ socket/client.c
@@ -7,40 +7,30 @@
 #include<netinet/in.h>
 
 #define MAXLINE 4096
+#define PORT 6666
 
 int main(int argc, char** argv) {
   int sockfd, n;
-  char recvline[4096], sendline[4096];
-  struct sockaddr_in servaddr;
+  char recvline[MAXLINE], sendline[MAXLINE];
 
   if(argc != 2){
     printf("没有输入<ipaddress>\n");
     exit(0);
   }
 
-  if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-    printf("create socket error: %s(errno: %d)\n", strerror(errno),errno);
-    exit(0);
-  }
+  sockfd = open_connectfd(argv[1], PORT);
 
-  memset(&servaddr, 0, sizeof(servaddr));
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_port = htons(6666);
-  if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0){
-    printf("inet_pton error for %s\n",argv[1]);
-    exit(0);
-  }
+  while(1) {
+    printf("send msg to server: \n");
+    fgets(sendline, MAXLINE, stdin);
 
-  if(connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0){
-    printf("connect error: %s(errno: %d)\n",strerror(errno),errno);
-    exit(0);
-  }
+    if(send(sockfd, sendline, strlen(sendline), 0) < 0){
+      printf("send msg error: %s(errno: %d)\n", strerror(errno), errno);
+      exit(0);
+    }
 
-  printf("send msg to server: \n");
-  fgets(sendline, 4096, stdin);
-  if(send(sockfd, sendline, strlen(sendline), 0) < 0){
-    printf("send msg error: %s(errno: %d)\n", strerror(errno), errno);
-    exit(0);
+    if (strcmp(sendline, "exit\n") == 0)
+      break;
   }
 
   close(sockfd);
